9fed16976047bf1446796035c0eb91dfc40f2246
JI-HUN KIM <procyonq@gmai.com>
2015-01-16 01:37:18 +0900

    Fix broken text
    Fix broken text

--- vimdic.sh
+++ vimdic.sh
@@ -2,6 +2,6 @@
 SET_DIR=~/utility/scripts/vimdic
 clear
 w3m -dump "http://small.dic.daum.net/search.do?q=$1" > $SET_DIR/dump_dic 2>&1
-grep -A1 "µè±â¹Ýº¹µè±â" $SET_DIR/dump_dic > $SET_DIR/result_dic
+grep -A1 "ë“£ê¸°ë°˜ë³µë“£ê¸°" $SET_DIR/dump_dic > $SET_DIR/result_dic
 echo $1
 sed -n "2p" $SET_DIR/result_dic
