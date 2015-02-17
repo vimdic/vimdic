15420987659678044637054705120f263ab035e1
JI-HUN KIM <procyonq@gmail.com>
2015-02-18 00:23:25 +0900

    using wget instead of w3m
    wget is lighter and smaller than w3m.\
    There is no needs to install w3m any more

--- vimdic.sh
+++ vimdic.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export LANG=ko_KR.UTF-8
 clear
-w3m -dump -no-cookie "http://small.dic.daum.net/search.do?q=$1 $2 $3" |\
-	    sed -n -e "/영어 사전/,/영어 사전 더보기/p" |\
-	    sed -e "/^영어 사전/d" |\
-	    sed -e "/^\[/d"
+wget -q -O - "http://small.dic.daum.net/search.do?q=$1 $2 $3" |\
+	     sed -n -e "/eng_sch/,/<\/section>/p" |\
+	     grep "link_txt\|txt_means_KUEK\|trans" | sed -e 's/\t//g' |\
+	     # Wrap [] to target word from string like
+	     # "???&q=love" class="link_txt" >love</a>"
+	     sed -e 's/"link_txt"[^>]*>/"link_txt">[ /g' |\
+	     sed -e 's/<\/a>/ ]<\/a>/g' |\
+	     sed -e 's/<[^>]*>//g'
