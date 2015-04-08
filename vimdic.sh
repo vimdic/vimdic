65acf3d40075d5d694534159863d143b43a85adc
JI-HUN KIM <procyonq@gmail.com>
2015-04-08 21:47:32 +0900

    Display examples of target word

--- vimdic.sh
+++ vimdic.sh
@@ -11,12 +11,21 @@ if [ "$1" == "-w" ]; then
 else
 	export LANG=ko_KR.UTF-8
 	clear
+
+	# Meaning
 	wget -q -O - "http://small.dic.daum.net/search.do?q=$TARGET" |\
-		     sed -n -e "/eng_sch/,/<\/section>/p" |\
-		     grep "link_txt\|txt_means_KUEK\|trans" | sed -e 's/\t//g' |\
-		     # Wrap [] to target word from string like
-		     # "???&q=love" class="link_txt" >love</a>"
-		     sed -e 's/"link_txt"[^>]*>/"link_txt">[ /g' |\
-		     sed -e 's/<\/a>/ ]<\/a>/g' |\
-		     sed -e 's/<[^>]*>//g'
+		sed -n -e "/eng_sch/,/<\/section>/p" |\
+		grep "link_txt\|txt_means_KUEK\|trans" | sed -e 's/\t//g' |\
+		# Wrap [] to target word from string like
+		# "???&q=love" class="link_txt" >love</a>"
+		sed -e 's/"link_txt"[^>]*>/"link_txt">[ /g' |\
+		sed -e 's/<\/a>/ ]<\/a>/g' |\
+		sed -e 's/<[^>]*>//g'
+
+	# Example
+	wget -q -O - "http://small.dic.daum.net/search.do?q=$TARGET&t=example&dic=eng" |\
+		grep "<daum:word" | sed -e 's/\t//g' |\
+		sed -e 's/^[^>]*</</g' |\
+		sed -e 's/<[^>]*>//g'
+
 fi
